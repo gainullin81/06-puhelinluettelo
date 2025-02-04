ef37457486c09b2d4f485fe8f755fb9c299e8bfb
alex <againullin8@gmail.com>
2025-02-04 16:34:22 +0200

    URL  Render

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseUrl = import.meta.env.PROD
-  ? "https://puhelinluettelo-backend-je1y.onrender.com/api/persons"
+  ? "https://puhelinluettelo-backend.onrender.com/api/persons"
   : "http://localhost:3001/api/persons";
 
 const getAll = () => {
