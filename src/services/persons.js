b0641a5547502f290f33f759ab81894cf69069cf
alex <againullin8@gmail.com>
2025-02-04 16:26:39 +0200

    render

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-// Используйте разные базовые URL для разработки и production
 const baseUrl = import.meta.env.PROD
-  ? "https://puhelinluettelo-backend.onrender.com/api/persons"
+  ? "https://puhelinluettelo-backend-je1y.onrender.com/api/persons"
   : "http://localhost:3001/api/persons";
 
 const getAll = () => {
